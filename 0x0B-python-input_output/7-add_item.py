1fc5059f733912401f5f80d8bba25e7286f1be22
Samsonasumu <samsonasumu@gmail.com>
2022-09-27 06:30:40 -0700

    load add save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Module that adds all arguments to a python list,
+and then saves them to a file
+
+Use your function ``save_to_json_file`` from ``5-save_to_json_file.py``
+Use your function ``load_from_json_file`` from ``6-load_from_json_file.p``y
+"""
+
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+# load existing list from filename
+try:
+    result = load_from_json_file(filename)
+except Exception:
+    result = []
+
+result += sys.argv[1:]
+
+# write new list to filename
+save_to_json_file(result, filename)
