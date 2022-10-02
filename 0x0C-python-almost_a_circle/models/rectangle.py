1d13267cac4938b49bfe8ad254643bc35cf59956
Samsonasumu <samsonasumu@gmail.com>
2022-10-02 04:01:35 -0700

    changes to the file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,8 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Represents a rectangle and inherits som attrs from base"""
+   """
+   class Rectangle inherits from Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """test"""
